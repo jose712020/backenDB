17262979c0c5cf8e103b11a850f4a1de88908ebe
jose712020 <150953639+jose712020@users.noreply.github.com>
2026-02-13 13:27:35 +0100

    Add files via upload

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,21 @@
 const { MongoClient } = require('mongodb'); 
 
-const url = 'mongodb://root:root@127.0.0.1:27017/?authSource=admin';
+const url = process.env.MONGO_URI || 'mongodb://root:root@127.0.0.1:27017/?authSource=admin';
+
 const client = new MongoClient(url);
 let db;
 
 const connectDB = async () => {
-    if (db) return db;
-    await client.connect();
-    db = client.db('dragonball_db'); 
-    console.log("Conectado a MongoDB");
-    return db;
+    try {
+        if (db) return db;
+        await client.connect();
+        db = client.db('dragonball_db'); 
+        console.log("Conectado a MongoDB con éxito");
+        return db;
+    } catch (error) {
+        console.error("Error conectando a MongoDB:", error);
+        process.exit(1); // Esto ayuda a Render a saber que el servicio falló
+    }
 };
 
 module.exports = { connectDB };
